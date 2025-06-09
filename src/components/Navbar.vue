02a04a6d60734dd2077159a905f51a2858d8c8b5
Jason <jeasonraphael11@gmail.com>
2025-06-09 15:21:37 +0800

    First commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="navbar dark:bg-backgroundDark dark:border-b dark:border-primary/10 sticky top-0 z-10 shadow-sm">
+    <nav class="flex-none lg:hidden">
+      <label @click="$emit('toggle-drawer')" class="btn btn-square btn-ghost">
+        <Icon icon="line-md:close-to-menu-alt-transition" class="inline-block w-5 h-5" />
+      </label>
+    </nav>
+
+    <section class="flex-1">
+      <div class="form-control">
+        <input type="text" placeholder="Search"
+          class="input input-bordered w-48 m1-2 md:w-auto dark:bg-white/5 dark:backdrop-blur-md dark:[--webkit-backdrop-filter:blur(10px)] dark:border-white/10" />
+      </div>
+    </section>
+
+    <!-- Theme toggle button -->
+    <section class="flex gap-6">
+      <button @click="$emit('toggle-theme')"
+        class="btn btn-ghost btn-circle dark:bg-white/5 dark:backdrop-blur-md dark:[--webkit-backdrop-filter:blur(10px)] dark:border-white/10">
+        <Icon v-if="isDark" icon="line-md:moon-alt-to-sunny-outline-loop-transition" class="w-8 h-8" />
+        <Icon v-else icon="line-md:sunny-outline-to-moon-alt-loop-transition" class="w-8 h-8" />
+      </button>
+
+      <div class="dropdown dropdown-end">
+        <button tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+          <div class="w-10 rounded-full">
+            <img src="/home/intern/Personal/vue-dashboard/vue-dashboard-test/user.png" alt="User">
+          </div>
+        </button>
+        <ul tabindex="0" class="mt-5 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box
+        w-58 space-y-3 dark:bg-backgroundDark **:font-semobold **:text-base">
+          <li><a>Profile</a></li>
+          <li><a>Settings</a></li>
+          <li><a>Log Out</a></li>
+        </ul>
+      </div>
+    </section>
+  </header>
+</template>
+<script setup>
+defineProps({
+  isDark: {
+    type: Boolean, required: true
+  }
+})
+
+defineEmits(["toggle-drawer", "toggle-theme"]);
+</script>
